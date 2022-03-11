729502304fd85b9dab07ac38a327c3b840c75a24
Kevin Cabrera <kevintroko@gmail.com>
2022-03-11 14:41:06 +0100

    Add actions logic without any redux or ngrx

--- 01-redux/app-1.ts
+++ 01-redux/app-1.ts
@@ -1,1 +1,47 @@
-console.log('Hello World');
+// Actions 
+interface Action {
+    type: string;
+    payload?: any;
+}
+
+enum ACTIONS {
+    INCREMENT = 'INCREMENT',
+    DECREMENT = 'DECREMENT',
+    MULTIPLY = 'MULTIPLY',
+    DIVIDE = 'DIVIDE',
+}
+
+/* --- Must always return state --- */
+const reducer = (state = 10, action: Action) => {
+    switch (action.type) {
+        case ACTIONS.INCREMENT:
+            return state + 1;
+        case ACTIONS.DECREMENT:
+            return state - 1;
+        case ACTIONS.MULTIPLY: 
+            return state * action.payload;
+        case ACTIONS.DIVIDE: 
+            return state / action.payload;
+        default: 
+            return state;
+    }
+}
+
+const incrementAction: Action = {
+    type: ACTIONS.INCREMENT,
+};
+
+const decrementAction: Action = {
+    type: ACTIONS.DECREMENT,
+};
+
+console.log(reducer(0, incrementAction));
+console.log(reducer(0, decrementAction));
+console.log(reducer(4, {
+    type: ACTIONS.MULTIPLY,
+    payload: 2,
+}));
+console.log(reducer(4, {
+    type: ACTIONS.DIVIDE,
+    payload: 2,
+}))
