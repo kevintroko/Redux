47a5c6e942593cd4866d81774f50b147eb58e6d3
Kevin Cabrera <kevintroko@gmail.com>
2022-03-14 10:34:14 +0100

    Add state function

---
+++ 01-redux/app-3.ts
@@ -0,0 +1,23 @@
+import { incrementAction } from "./counter/counter.actions";
+import { counterReducer } from "./counter/counter.reducer";
+import { Action, Reducer } from "./ngrx-fake/ngrx";
+
+class Store<T> {    
+    constructor(private reducer: Reducer<T>, private state: T) {
+
+    }
+
+    getState(): T {
+        return this.state;
+    }
+
+    dispatch(action: Action): void {
+        this.state = this.reducer(this.state, action);
+    }
+}
+
+const store = new Store(counterReducer, 0);
+
+console.log(store.getState());
+store.dispatch(incrementAction);
+console.log(store.getState());
