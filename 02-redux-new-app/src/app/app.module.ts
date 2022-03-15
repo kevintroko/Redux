5ef58bceeefefb4eea2e445e487da00ed9652a90
Kevin Cabrera <kevintroko@gmail.com>
2022-03-15 10:20:53 +0100

    Add debuggin options

--- 02-redux-new-app/src/app/app.module.ts
+++ 02-redux-new-app/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { SonComponent } from './counter/son/son.component';
 import { GrandsonComponent } from './counter/grandson/grandson.component';
 import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter/counter.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment'; // Angular CLI environment
 
 @NgModule({
   declarations: [
@@ -17,6 +19,11 @@ import { counterReducer } from './counter/counter.reducer';
     BrowserModule,
     StoreModule.forRoot({
       counter: counterReducer
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
     }),
   ],
   providers: [],
