8731cef22959ae83bb6882cba12d8c7d4616920a
Kevin Cabrera <kevintroko@gmail.com>
2022-03-15 14:24:18 +0100

    Add rest of files

--- 03-todos/src/app/todos/todo.module.ts
+++ 03-todos/src/app/todos/todo.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoAddComponent } from './todo-add/todo-add.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoPageComponent } from './todo-page/todo-page.component';
 
 
 
 @NgModule({
   declarations: [
-    TodoAddComponent
+    TodoAddComponent,
+    TodoItemComponent,
+    TodoListComponent,
+    TodoPageComponent
   ],
   imports: [
     CommonModule
